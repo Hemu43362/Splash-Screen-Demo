ab5d14864364305c644c7e9b1fe15fe7956dd91e
Hemu43362 <hemu.4462@gmail.com>
2021-03-11 19:23:43 +0530

    initial commit

---
+++ app/src/main/java/com/hemantpatel/splashscreendemo/SplashScreenActivity.java
@@ -0,0 +1,33 @@
+package com.hemantpatel.splashscreendemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    Handler mHandler;
+    Runnable mRunnable;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        mHandler = new Handler();
+        mRunnable = new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+
+        // its trigger runnable after 4000 millisecond.
+        mHandler.postDelayed(mRunnable,4000);
+
+    }
+
+}
